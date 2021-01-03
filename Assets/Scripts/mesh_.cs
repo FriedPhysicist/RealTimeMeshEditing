494c0523fcd927ab70812303f783c886b05a425d
FriedPhysicist <59540097+FriedPhysicist@users.noreply.github.com>
2021-01-03 05:06:24 +0300

    GF

---
+++ Assets/Scripts/mesh_.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mesh_ : MonoBehaviour
+{ 
+    public Mesh orginal_mesh; 
+    public Vector3[] orginal_mesh_verticies;
+    public GameObject sphere;
+
+
+
+    void Start()
+    { 
+        orginal_mesh=gameObject.GetComponent<MeshFilter>().mesh; 
+        System.Array.Resize(ref orginal_mesh_verticies, orginal_mesh.vertices.Length); 
+        orginal_mesh_verticies=orginal_mesh.vertices; 
+
+        foreach(Vector3 vert in orginal_mesh.vertices)
+        { 
+            //Instantiate(sphere,transform.TransformPoint(vert),Quaternion.identity);
+        }
+    }
+
+
+    void Update()
+    { 
+        orginal_mesh.vertices=orginal_mesh_verticies; 
+
+        foreach(Vector3 vert in orginal_mesh.vertices)
+        {
+
+        }
+    }
+    void OnTriggerStay(Collider other) 
+    { 
+        if(other.CompareTag("sphere"))
+        { 
+            for(int i=0;i<orginal_mesh_verticies.Length;i++)
+            { 
+                if(other.bounds.Contains(transform.TransformPoint(orginal_mesh_verticies[i])))
+                { 
+                    //orginal_mesh_verticies[i]-=new Vector3(0,0,2);
+                    System.Array.Clear(orginal_mesh.vertices,i,i);
+                }
+
+            } 
+        }
+    }
+}
+
