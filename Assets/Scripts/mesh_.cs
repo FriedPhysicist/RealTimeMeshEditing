358bbd99d886694df1dc3034f64e012d787020df
FriedPhysicist <59540097+FriedPhysicist@users.noreply.github.com>
2021-02-17 23:46:39 +0300

    GF

--- Assets/Scripts/mesh_.cs
+++ Assets/Scripts/mesh_.cs
@@ -1,39 +1,56 @@
-﻿using System.Collections;
+﻿using System; 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class mesh_ : MonoBehaviour
 { 
-    public Mesh orginal_mesh; 
-    public GameObject sphere;
+    public Mesh orginal_mesh;
+
+    GameObject bucket;
+    Mesh bucket_mesh;
+    public GameObject planee;
+
+    public static Vector3 current_pos;
 
 
     public Vector3[] orginal_mesh_verticies;
     public Vector2[] orginal_mesh_uv;
-    public int[] orginal_mesh_triangels;
-
-    public RenderTexture RT;
-
-
-
-
+    public int[] orginal_mesh_triangels; 
 
     void Start()
     { 
         orginal_mesh=gameObject.GetComponent<MeshFilter>().mesh; 
+        bucket = GameObject.FindWithTag("bucket");
+        bucket_mesh = bucket.GetComponent<MeshFilter>().mesh;
 
-        System.Array.Resize(ref orginal_mesh_verticies, orginal_mesh.vertices.Length); 
-        System.Array.Resize(ref orginal_mesh_uv, orginal_mesh.uv.Length); 
-        System.Array.Resize(ref orginal_mesh_triangels, orginal_mesh.triangles.Length); 
+        Array.Resize(ref orginal_mesh_verticies, orginal_mesh.vertices.Length); 
+        Array.Resize(ref orginal_mesh_uv, orginal_mesh.uv.Length); 
+        Array.Resize(ref orginal_mesh_triangels, orginal_mesh.triangles.Length); 
 
         orginal_mesh_verticies=orginal_mesh.vertices; 
         orginal_mesh_uv=orginal_mesh.uv; 
-        orginal_mesh_triangels=orginal_mesh.triangles; 
+        orginal_mesh_triangels=orginal_mesh.triangles;
+
+        GameObject my_object = new GameObject();
+
+        my_object.AddComponent<MeshFilter>().mesh.vertices = bucket_mesh.vertices; 
+        my_object.AddComponent<MeshRenderer>().material.color=Color.blue; 
     }
 
 
     void Update()
     { 
+        Debug.DrawRay(Camera.main.transform.position,Camera.main.transform.forward*5);
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward * 5);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        { 
+            current_pos=planee.transform.InverseTransformPoint(hit.point);
+            Debug.Log(hit.point);
+        }
+        
         orginal_mesh.vertices=orginal_mesh_verticies; 
         orginal_mesh.uv=orginal_mesh_uv; 
         orginal_mesh.triangles=orginal_mesh_triangels; 
@@ -47,12 +64,11 @@ public class mesh_ : MonoBehaviour
             for(int i=0;i<orginal_mesh_verticies.Length;i++)
             { 
                 if(other.bounds.Contains(transform.TransformPoint(orginal_mesh_verticies[i])))
-                { 
+                {
                     orginal_mesh_verticies[i]=new Vector3(orginal_mesh_verticies[i].x,orginal_mesh_verticies[i].y,-0.001f); 
                     orginal_mesh_uv[i]=new Vector2(0,0); 
                 } 
             } 
         }
     }
-}
-
+} 
---
+++ Assets/drawer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawer : MonoBehaviour
+{
+    public GameObject Sprite;
+    public bool on_off=false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mesh_.current_pos;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            on_off = true; 
+        } 
+        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            on_off = false;
+        }
+
+        while (on_off)
+        {
+            GameObject some = Instantiate(Sprite, transform.position, Quaternion.identity);
+            break;
+        }
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("spray") && !on_off)
+        { 
+            Destroy(other.gameObject,0);
+        }
+    }
+}
