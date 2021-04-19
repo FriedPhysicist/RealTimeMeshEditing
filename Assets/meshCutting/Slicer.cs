794c3f19c02a72061f61bb80af9bdfae0004e3d1
FriedPhysicist <59540097+FriedPhysicist@users.noreply.github.com>
2021-04-19 22:21:20 +0300

    ME

---
+++ Assets/meshCutting/Slicer.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Slicer : MonoBehaviour 
+{
+    public GameObject[] tryholdvertices;
+    public Vector3[] vertices;
+
+    public GameObject try_;
+
+    Vector3 _in;
+    Vector3 _out;
+
+    public GameObject plane;
+
+    void Start()
+    {
+	vertices = plane.GetComponent<MeshFilter>().mesh.vertices;
+    }
+
+    void Update()
+    {
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+	if(other.CompareTag("Slicable"))
+	{
+	    Mesh oMesh=other.GetComponent<MeshFilter>().mesh;
+	    _in=transform.position;
+	}
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+	if(other.CompareTag("Slicable"))
+	{
+	    Mesh oMesh=other.GetComponent<MeshFilter>().mesh;
+	    Material oMaterial=other.GetComponent<MeshRenderer>().material;
+	    _out=transform.position;
+
+	    GameObject go0 = new GameObject();
+	    Mesh go0Mesh = new Mesh();
+
+	    go0Mesh.vertices = new Vector3[]
+	    {
+		//other.transform.TransformPoint(
+		oMesh.vertices[120],
+		oMesh.vertices[40],
+		_out,
+		_in 
+	    };
+
+	    go0Mesh.triangles = new int[]
+	    {
+		0,1,2,
+		1,3,2
+	    };
+
+	    go0Mesh.RecalculateBounds();
+	    go0Mesh.RecalculateNormals();
+	    go0Mesh.RecalculateTangents();
+
+
+	    go0.AddComponent<MeshRenderer>().material= oMaterial;
+	    go0.AddComponent<MeshFilter>().mesh= go0Mesh;
+	    go0.AddComponent<MeshCollider>().convex= true;
+
+	    Destroy(other.gameObject, 0f);
+
+
+	    GameObject go1 = new GameObject();
+	}
+    }
+
+
+
+
+
+
+
+
+
+
+    void meshCreation()
+    {
+	tryholdvertices = GameObject.FindGameObjectsWithTag("bucket");
+	System.Array.Resize(ref vertices, tryholdvertices.Length);
+
+	for(int i = 0; i<10 ; i++)
+	{
+	    vertices[i] = tryholdvertices[i].transform.position;
+	}
+
+	try_ = new GameObject();
+	try_.transform.position = new Vector3(10,2.5f,0.4f);
+
+	Mesh tryMesh = new Mesh();
+	tryMesh.vertices = vertices;
+
+	tryMesh.triangles = new int[]
+	{
+	    0,1,6,
+	    1,2,7,
+	    2,3,8,
+	    3,4,9,
+	    4,5,0,
+	    5,6,1,
+	    6,7,2,
+	    7,8,3,
+	    8,9,4,
+	    9,0,5
+	};
+
+	try_.AddComponent<MeshRenderer>().material = GetComponent<MeshRenderer>().material;
+	try_.AddComponent<MeshFilter>().mesh= tryMesh;
+	try_.AddComponent<MeshCollider>().convex= true;
+
+	tryMesh.RecalculateBounds();
+	tryMesh.RecalculateNormals();
+	tryMesh.RecalculateTangents();
+    }
+}
--- Assets/meshCutting/getValues.cs
+++ Assets/meshCutting/getValues.cs
@@ -3,39 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter))]
 public class getValues : MonoBehaviour
 {
-    
-    Mesh MF;
-
     public Vector3[] vertices_copy;
     public int[] triangles_copy;
+    
+    Mesh MF;
+    Mesh someMesh;
+
+    public int xSize;
+    public int zSize;
+
+    public Transform holder;
 
     void Start()
     {
+	someMesh = GetComponent<MeshFilter>().mesh;
+
+	vertices_copy  = GetComponent<MeshFilter>().mesh.vertices;
+	triangles_copy = GetComponent<MeshFilter>().mesh.triangles;
+	
+    }
+
+    void Update()
+    {
+	Debug.Log(transform.TransformPoint(someMesh.bounds.ClosestPoint(holder.position)));
+    }
+
+    int some = 0;
+    void createShapeArray()
+    {
 	MF = new Mesh();
 
-	triangles_copy = new int[]
-	{
-	    0,2,1,
-	    2,3,1,
-	    4,6,5,
-	    6,7,5
-	};
+	vertices_copy = new Vector3[(xSize+1)*(zSize+1)];
+	triangles_copy = new int[xSize*zSize*6];
 
-	vertices_copy = new Vector3[]
-	{
-	    new Vector3(0,0,0), //left bottom
-	    new Vector3(1,0,0), //right bottom
-	    new Vector3(0,0,1), //legt top
-	    new Vector3(1,0,1),  //right top
-	    new Vector3(1,1,1),
-	    new Vector3(1,1,1),
-	    new Vector3(1,1,1),
-	    new Vector3(1,1,1)
-	};
 
+	for(int i = 0, z = 0; z<= zSize; z++)
+	{
+	    for(int x = 0; x<= xSize; x++)
+	    {
+		vertices_copy[i] = new Vector3(x,0,z);
+		i++;
+	    }
+	}
+
+	int vert = 0;
+	int tris = 0;
+
+	for(int i = 0; i< zSize; i++)
+	{
+	    for(int x = 0; x< xSize; x++)
+	    {
+		triangles_copy[tris+0] = vert + 0;
+		triangles_copy[tris+1] = vert + xSize + 1;
+		triangles_copy[tris+2] = vert + 1;
+		triangles_copy[tris+3] = vert + 1;
+		triangles_copy[tris+4] = vert + xSize + 1;
+		triangles_copy[tris+5] = vert + xSize + 2;
+
+		vert++;
+		tris+=6;
+	    }
+	}
 
 	MF.vertices = vertices_copy;
 	MF.triangles = triangles_copy;
@@ -44,13 +74,11 @@ public class getValues : MonoBehaviour
 	GetComponent<MeshFilter>().mesh.RecalculateNormals();
     }
 
-    void Update()
-    {
-	MF.vertices = vertices_copy;
-	MF.triangles = triangles_copy;
-    }
-
     void OnDrawGizmos()
     {
+	for(int i = 0; i<vertices_copy.Length; i++)
+	{
+	    //Gizmos.DrawSphere(MF.vertices[i], 0.1f);
+	}
     }
 }
